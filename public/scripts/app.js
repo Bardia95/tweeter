0d180725114496538d362a56f570391dc4c4e08f
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-06-19 18:25:54 -0400

    fixed highlighting on hover

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  $(".tweet").hover(function() {
+    $(this).addClass("highlight");
+  }, function(){
+    $(this).removeClass("highlight");
+  });
+  $(".fas").hover(function() {
+    console.log(this);
+    $(this).addClass("iconHighlight");
+  }, function() {
+    console.log(this);
+    $(this).removeClass("iconHighlight");
+  });
+});
+
+
+
+
