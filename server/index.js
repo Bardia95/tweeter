e2c99da30d04a9d6cbaa4c97d9c648307ff6e752
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-06-22 14:12:59 -0400

    mongodb done, added scss/path

--- server/index.js
+++ server/index.js
@@ -6,9 +6,17 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+const nodeSassMiddleware = require('node-sass-middleware');
+const path = require('path');
 
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'scss'),
+    dest: path.join(__dirname, '../public/styles'),
+    debug: true,
+    outputStyle: 'compressed',
+}));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 const {MongoClient} = require("mongodb");
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -8,7 +8,7 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-        db.tweets.push(newTweet);
+        db.collection("tweets").insertOne(newTweet);
         callback(null, true);
     },
 
