3a74d6950783b350c92276dfecdc29af077617bb
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-06-22 17:37:21 -0400

    fixing port

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // Basic express setup:
 
-const PORT          = 8080;
+const PORT          = 5000;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
