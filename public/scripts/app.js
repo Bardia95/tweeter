f2e55a05cbc35db854499992d6ec57ec54874a60
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-06-24 00:29:11 -0400

    fixed reset char

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -55,7 +55,6 @@ $(document).ready(function() {
     let errors = $(this).siblings('#submiterrors')
     errors.empty();
     let tweettext = $(this).find('textarea').val().trim();
-    console.log(tweettext);
     // Form validation, not null, not empty, not over char limit
     if (tweettext != null && tweettext != "" && tweettext.length <= 140) {
       $.ajax({
@@ -66,6 +65,7 @@ $(document).ready(function() {
         loadTweets();
       });
       $(this).find('textarea').val('');
+      $(this).find('.counter').html('140');
     } else if (tweettext == null || tweettext == "") {
       errors.append("<p>Tweet can't be empty or null</p>");
     } else if (tweettext.length > 140) {
